28eb6160cf4f938ef3ef6a36a365096b8bc71252
Ucenik <Ucenik@DESKTOP-EDUBNMJ>
2023-11-14 09:34:09 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ProvjeraNiOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Color = new System.Windows.Forms.Button();
+            this.Position = new System.Windows.Forms.Button();
+            this.Size = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Color
+            // 
+            this.Color.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Color.Location = new System.Drawing.Point(566, 36);
+            this.Color.Name = "Color";
+            this.Color.Size = new System.Drawing.Size(75, 23);
+            this.Color.TabIndex = 0;
+            this.Color.Text = "Color";
+            this.Color.UseVisualStyleBackColor = true;
+            this.Color.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // Position
+            // 
+            this.Position.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Position.Location = new System.Drawing.Point(566, 156);
+            this.Position.Name = "Position";
+            this.Position.Size = new System.Drawing.Size(75, 23);
+            this.Position.TabIndex = 1;
+            this.Position.Text = "Position";
+            this.Position.UseVisualStyleBackColor = true;
+            this.Position.Click += new System.EventHandler(this.Position_Click);
+            // 
+            // Size
+            // 
+            this.Size.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Size.Location = new System.Drawing.Point(566, 288);
+            this.Size.Name = "Size";
+            this.Size.Size = new System.Drawing.Size(75, 23);
+            this.Size.TabIndex = 2;
+            this.Size.Text = "Size";
+            this.Size.UseVisualStyleBackColor = true;
+            this.Size.Click += new System.EventHandler(this.Size_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(187, 289);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Size);
+            this.Controls.Add(this.Position);
+            this.Controls.Add(this.Color);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Color;
+        private System.Windows.Forms.Button Position;
+        private System.Windows.Forms.Button Size;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProvjeraNiOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Color_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void Size_Click(object sender, EventArgs e)
+        {
+            int a;
+            if (int.TryParse(textBox3.Text, out a))
+            {
+                this.Width += a;
+                this.Height += a;
+            }
+        }
+
+        private void Position_Click(object sender, EventArgs e)
+        {
+            int x;
+            if (int.TryParse(textBox2.Text, out x))
+            {
+                Position.Left += x;
+                textBox2.Left += x;
+            }
+            else
+            {
+                MessageBox.Show("Unesite valjan broj.");
+            }
+        
+        }
+    }
+}
